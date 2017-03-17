0e022ae744a050158c6db671d966845717f41acb
shesee <sheseee@gmail.com>
2017-03-17 11:56:41 +0800

    refactor code structure

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ module.exports = function(from_email, to_email, title, content) {
     to_email = typeof to_email !== 'undefined' ? to_email : "test@example.com";
     title = typeof title !== 'undefined' ? title : "Sending with SendGrid is Fun";
     content = typeof content !== 'undefined' ? content : "and easy to do anywhere, even with Node.js";
-    var sendgrid = require('./lib/sendgrid.js');
-    sendgrid.send(from_email, to_email, title, content);
+    var mailer = require('./lib/mailer.js');
+    mailer.send(from_email, to_email, title, content);
 };
