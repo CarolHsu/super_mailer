df893658aefff19955d1fa783349a07247ef22f9
shesee <sheseee@gmail.com>
2017-03-23 00:03:54 +0800

    improve request test

--- tests/lib/mailer_spec.js
+++ tests/lib/mailer_spec.js
@@ -1,12 +1,25 @@
 var assert = require('assert');
+var sinon  = require('sinon');
+var request = require('request');
 var mailer = require('../../lib/mailer.js');
 describe('#send', function(){
-    var from_email, to_email, title, content, api;
     beforeEach(function() {
-        from_email = "test@example.com";
-        to_email = "test@example.com";
-        subject = "Sending with SendGrid is Fun";
-        content = "and easy to do anywhere, even with Node.js";
-        //mailer.send(from_email, to_email, subject, content);
+        from_email = "test@example.com"
+        to_email = "test@example.com"
+        subject = "test"
+        content = "test"
+        sinon.spy(request, 'post');
+    });
+    afterEach(function() {
+        request.post.restore();
+    });
+
+    it('should post http request without any callback', function() {
+        var data = {to: "test", from: "test"}
+        var options = {method: 'post', url: "http://test.com", json: JSON.stringify(data) }
+        var callback = sinon.spy();
+        var proxy = mailer.send_with_data(options, from_email, to_email, subject, content, callback);
+
+        assert(callback.notCalled);
     });
 });
